cfa2516174a738b7a4a61b59e556a7cf7bdfc5d6
Ivars <ivars.melecis@gmail.com>
2019-10-01 17:51:06 +0100

    white vaio commit

--- src/components/VerticalMenu.jsx
+++ src/components/VerticalMenu.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useContext } from "react";
+import HomeContext from "../contexts/HomeContext";
 
 const VerticalMenu = () => {
   return <h1>Vertical Menu</h1>;
